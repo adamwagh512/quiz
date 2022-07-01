44559ada7783dd05328429b8de1f8f9d4bfe427e
Adam Wagh <adamwagh@gmail.com>
2022-07-01 08:35:30 -0500

    Built home screen, main question page, and added some styling. Need to start building out the javascript

---
+++ assets/javascript/style.js
@@ -0,0 +1,12 @@
+
+
+var startButton = document.querySelector('#gamestart');
+var homeScreen = document.getElementById('homescreen');
+
+startButton.addEventListener('click', function() {
+    console.log('test')
+});
+
+function q1 () {
+    
+}
